e25874d3b8244006f10bb50e84203c443ac01f13
Levin E P <levin.padmarajan@qburst.com>
2024-06-24 00:12:43 +0530

    initial commit

---
+++ src/main/java/com/edubuddy/edubuddy/EdubuddyApplication.java
@@ -0,0 +1,13 @@
+package com.edubuddy.edubuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdubuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdubuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edubuddy/edubuddy/EdubuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edubuddy.edubuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdubuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
